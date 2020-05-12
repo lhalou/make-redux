842469237bc11d5461ebc203cc388ea84661b9c1
lliqi <614124284@qq.com>
2020-05-12 18:28:56 +0800

    add

---
+++ build/precache-manifest.eaa8db28c0c912fa90a119e48872b628.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108f92ae4664bc81392ae90540ca8f13",
+    "url": "./index.html"
+  },
+  {
+    "revision": "28aa6feb501afc636d69",
+    "url": "./static/js/main.ab2cec41.chunk.js"
+  },
+  {
+    "revision": "33a271edff95311562b1",
+    "url": "./static/js/runtime-main.c09a9eda.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,88 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+function createStore(reducer) {
+  let state = null;
+  const listeners = [];
+  const subscribe = (listener) => listeners.push(listener);
+  const getState = () => state;
+  //订阅者模式 更改数据自动渲染
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  };
+  dispatch({}); // 初始化 state
+  return { getState, dispatch, subscribe };
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function renderApp(newAppState, oldAppState = {}) {
+  if (newAppState === oldAppState) return;
+  console.log("render app");
+  renderTitle(newAppState.title, oldAppState.title);
+  renderContent(newAppState.content, oldAppState.content);
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function renderTitle(newTitle, oldTitle = {}) {
+  if (newTitle === oldTitle) return;
+  console.log("render title");
+  const titleDOM = document.getElementById("title");
+
+  titleDOM.innerHTML = newTitle.text;
+  titleDOM.style.color = newTitle.color;
+}
+
+function renderContent(newContent, oldContent = {}) {
+  if (newContent === oldContent) return;
+  console.log("render content");
+  const contentDOM = document.getElementById("content");
+  contentDOM.innerHTML = newContent.text;
+  contentDOM.style.color = newContent.color;
+}
+
+let appState = {};
+
+function reducer(state, action) {
+  if (!state) {
+    return {
+      title: {
+        text: "React小书",
+        color: "red",
+      },
+      content: {
+        text: "React小书内容",
+        color: "blue",
+      },
+    };
+  }
+  //采用共同结构对象，防止渲染整个页面
+  switch (action.type) {
+    case "UPDATE_TITLE_TEXT":
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text,
+        },
+      };
+      break;
+    case "UPDATE_TITLE_COLOR":
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color,
+        },
+      };
+      break;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+let oldState = store.getState();
+store.subscribe(() => {
+  const newState = store.getState();
+  renderApp(newState, oldState);
+  oldState = newState;
+});
+renderApp(store.getState()); //首次渲染页面
+store.dispatch({ type: "UPDATE_TITLE_TEXT", text: "《React.js小书》" });
+store.dispatch({ type: "UPDATE_TITLE_COLOR", color: "pink" });
